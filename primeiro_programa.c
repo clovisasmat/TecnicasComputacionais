df8f834ecb9ca20166da5ec3a717cef13040b160
Clovis Antonio da Silva <66483865+clovisasmat@users.noreply.github.com>
2022-03-19 18:07:12 -0300

    Add files via upload
    upload do primeiro programa em C

---
+++ primeiro_programa.c
@@ -0,0 +1,24 @@
+/*Tecnicas computacionais 2022-1
+  Primeiro programa em C
+  01/02/2022*/
+
+//biblioteca de entrada-saida e biblioteca-padrao
+#include<stdio.h>
+#include<stdlib.h>
+
+//prototipo da funcao hello
+void hello();
+
+//funcao principal main
+int main(){
+  printf("Hello, world\n");//imprime hello, world na tela
+
+  hello();
+
+  return 0;
+}
+
+//funcao que imprime hello, world na tela
+void hello(){
+  printf("Hello, world\n");
+}
